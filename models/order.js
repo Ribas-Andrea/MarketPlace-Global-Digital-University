5a34495b761f3c5bfe9ca74f7246ef19e91638ec
Ribas Andréa <ribas.andrea17800@gmail.com>
2026-07-28 16:41:09 +0200

    Ajout du model, puis des routes CRUD et préparation des fonctions CRUD

--- models/order.js
+++ models/order.js
@@ -1,26 +1,38 @@
 const mongoose = require ('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  // image: {type: String, required: true},
-  // nom: {type: String, required: true},
-  // prix: {type: Number, required: true},
-  // categorie: {type: String, required: true},
-  // options: {
-  //   taille: {
-  //     type: String, 
-  //     enum: ['Menu Best Of', 'Menu Maxi Best Of'],
-  //     required: true},
-  //   accompagnement: {
-  //     type: String, 
-  //     enum: ['Frites', 'Potatoes'],
-  //     required: true},
-  //   boisson: {
-  //     type: String, 
-  //     required: true},
-  //   sauce: {
-  //     type: String, 
-  //     required: true}
-  // }
+  titre: {
+    quantité: {
+      type: Number, 
+      required: true
+    },
+    taille: {
+      type: String, 
+      enum: ['Menu Best Of', 'Menu Maxi Best Of'],
+      required: true
+    },
+    nom: {
+      type: String,
+      required: true
+    }
+  },
+  options: {
+    accompagnement: {
+      type: String, 
+      enum: ['Frites', 'Potatoes'],
+      required: true
+    },
+    boisson: {
+      type: String, 
+      required: true
+    },
+    sauce: {
+      type: String, 
+      required: true
+    },
+  },
+  image: {type: String, required: true},
+  prix: {type: Number, required: true}
 }, {timestamps: true});
 
 
