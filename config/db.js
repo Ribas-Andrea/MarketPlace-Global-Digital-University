0eb58503d1b53bf8b326c46ec56f8e6e70cda092
Ribas Andréa <ribas.andrea17800@gmail.com>
2026-08-14 16:00:06 +0200

    debug: log mongodb connection error

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rdfu7fa.mongodb.net/marketplace?retryWrites=true&w=majority&appName=Cluster0`);
     console.log('MongoDB connectée');
   } catch (err) {
-    console.error(err.message);
+    console.error('Erreur MongoDB :', err);
   }
 };
 
