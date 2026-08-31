a24a0ca27f119df950b3f0fe50fd7d680b9ebaf4
Ribas Andréa <ribas.andrea17800@gmail.com>
2026-08-31 09:59:58 +0200

    Modification middlexare auth pour afficher une erreur 401 lorsqu'il n'y a pas de token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,32 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const token = req.header('Authorization').split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Veuillez vous connecter' });
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: 'Veuillez vous connecter'
+    });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Veuillez vous connecter'
+    });
+  }
 
   try {
     const decodedUser = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decodedUser;
+
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'Veuillez vous connecter' });
+    return res.status(401).json({
+      message: 'Veuillez vous connecter'
+    });
   }
 };
 
